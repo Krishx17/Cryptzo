8243a8c8653144d54a75f9603713f05659443dfa
rudy45kc <krishneshchaurasia1@gmail.com>
2024-06-30 02:56:07 +0530

    timer bug fixed

--- public/app.js
+++ public/app.js
@@ -1,17 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const fetchAndRender = async () => {
     try {
-      const response = await fetch('/api/tickers');
+      const response = await fetch("/api/tickers");
       // console.log(response)
-      if(!response.ok){ 
-          throw new Error(`HTTP error! status ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status ${response.status}`);
       }
       const tickers = await response.json();
       // console.log(response);
-      // console.log(tickers);
-      const app = document.getElementById('app');
-      app.innerHTML = ''
-      const table = document.createElement('table');
+      console.log(tickers);
+      const bestPrice = document.getElementById("best-price-info");
+      const avgBuy = tickers[0].buy;
+      bestPrice.innerHTML = "₹" + avgBuy;
+
+      const app = document.getElementById("app");
+      app.innerHTML = "";
+      const table = document.createElement("table");
       // alert("Page refreshed");
       const header = `
         <tr>
@@ -19,46 +23,51 @@ document.addEventListener('DOMContentLoaded', () => {
           <th>Platform</th>
           <th>Last Traded Price</th>
           <th>Buy / Sell Price</th>
-          <th>Sell</th>   
-          <th>Volume</th>
-          <th>Base Unit</th>
+          <th>Difference</th>
+          <th>Savings</th>
         </tr>
       `;
       table.innerHTML = header;
-    
-      tickers.forEach(ticker => {
+
+      tickers.forEach((ticker) => {
         const row = `
           <tr>
             <td>${ticker.id}</td>
             <td>${ticker.name}</td>
-            <td>${"₹"+Number(ticker.last).toFixed(2)}</td>
-            <td>${"₹"+Number(ticker.buy).toFixed(2)}</td>
-            <td>${"₹"+Number(ticker.sell).toFixed(2)}</td>
-            <td>${Number(ticker.volume).toFixed(2)}</td>
-            <td>${ticker.base_unit}</td>
+            <td>${"₹" + Number(ticker.last).toFixed(2)}</td>
+            <td>${"₹" + ticker.buy + "  /  ₹" + ticker.sell}</td>
+            <td class="${(ticker.sell - ticker.buy)/100 > 0 ? 'positive' : 'negative'}">${(ticker.sell - ticker.buy) / 100 > 0 ? "" : "- "}${Number((ticker.sell - ticker.buy) / 100).toFixed(2) + " %"}</td>
+            <td class="${(ticker.sell - ticker.buy) > 0 ? 'positive' : 'negative'}">${(ticker.sell - ticker.buy) > 0 ? '▲' : '▼'}${"₹" + Number(ticker.sell - ticker.buy).toFixed(2)}</td>
           </tr>
         `;
         table.innerHTML += row;
       });
-    
+
+      // <td class="${profitLoss >= 0 ? 'positive' : 'negative'}">₹${Math.abs(profitLoss).toFixed(2)} ${profitLoss >= 0 ? '▲' : '▼'}</td>
+
       app.appendChild(table);
-      function startTimer(duration, display) {
-        let timer = duration, seconds;
-        setInterval(function () {
-            seconds = parseInt(timer, 10);
 
-            display.textContent = seconds;
-
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
-    }
     } catch (err) {
-        console.error('Error fetching tickers : ', err);
+      console.error("Error fetching tickers : ", err);
     }
-  }
+  };
+
+  let count = 60;
+  const updateTimer = () => {
+    const timerDisplay = document.getElementById("indicator");
+    
+    if (timerDisplay) { 
+      timerDisplay.textContent = count + " seconds remaining";
+      count--;
+      if (count == 0) {
+        count = 60;
+      }
+    }
+  };
+
   fetchAndRender();
-  setInterval(fetchAndRender(), 60000);
+  setInterval(fetchAndRender, 60000); 
+  setInterval(updateTimer, 1000); 
 });
+
 
--- server.js
+++ server.js
@@ -1,42 +1,47 @@
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const { Sequelize, DataTypes } = require('sequelize');
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const app = express();
 const port = 3009;
 
-//Middleware 
+//Middleware
 app.use(bodyParser.json());
-app.use('/home', express.static(__dirname + '/public')); // Add this line to serve static files
+app.use("/home", express.static(__dirname + "/public")); // Add this line to serve static files
 
 //Database setup
-const sequelize = new Sequelize('postgres://postgres:Jaishridev009@localhost:5432/hodlinfo', {
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(
+    "postgres://postgres:Jaishridev009@localhost:5432/hodlinfo",
+    {
+        dialect: "postgres",    
+    },
+);
 
-const Ticker = sequelize.define('Ticker', {
-    name: {type: DataTypes.STRING},
-    last: {type: DataTypes.FLOAT},
-    buy: {type: DataTypes.FLOAT},
-    sell: {type: DataTypes.FLOAT},
-    volume: {type: DataTypes.FLOAT},
-    base_unit: {type: DataTypes.STRING}
+const Ticker = sequelize.define("Ticker", {
+    name: { type: DataTypes.STRING },
+    last: { type: DataTypes.FLOAT },
+    buy: { type: DataTypes.FLOAT },
+    sell: { type: DataTypes.FLOAT },
+    volume: { type: DataTypes.FLOAT },
+    base_unit: { type: DataTypes.STRING },
 });
 
 const fetchData = async () => {
     try {
-        const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
+        const response = await axios.get(
+            "https://api.wazirx.com/api/v2/tickers",
+        );
         const data = response.data;
         // console.log(data);
         const top10 = Object.values(data).slice(0, 10);
         // console.log(top10);
 
-        await Ticker.sync({ force: true});
+        await Ticker.sync({ force: true });
         for (const item of top10) {
             await Ticker.create({
                 name: item.name,
-                last: item.last, 
+                last: item.last,
                 buy: item.buy,
                 sell: item.sell,
                 volume: item.volume,
@@ -48,18 +53,18 @@ const fetchData = async () => {
     }
 };
 
-app.get('/api/tickers', async (req, res) => {
-    try {   
+app.get("/api/tickers", async (req, res) => {
+    try {
         const tickers = await Ticker.findAll();
         // console.log(tickers);
         // res.json(tickers);
-        // res.sendFile(path.join(__dirname, 'index.html')); 
+        // res.sendFile(path.join(__dirname, 'index.html'));
         // res.json({
         //     status:"success",
         //     data:{
         //         tickers,
-        //     }    
-        // })  
+        //     }
+        // })
         res.send(tickers);
     } catch (err) {
         res.status(500).send("Error fetching data");
@@ -68,6 +73,6 @@ app.get('/api/tickers', async (req, res) => {
 
 //start server
 app.listen(port, () => {
-    console.log(`Server running on https://localhost: ${port}`);
+    console.log(`Server is up 👾🧮🚀✅ and is running on https://localhost: ${port}`);
     fetchData();
-})
+});
